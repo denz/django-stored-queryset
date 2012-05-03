35d10995ae6c069ffb888073f84328c828eddf6e
Denis Mishchishin <dennz78@gmail.com>
2012-05-03 11:38:18 +0300

    license/setup files names fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def package_env(file_name, strict=False):
     else:
         return u''
 
-PROJECT = u'django-stored-querys'
+PROJECT = u'django-stored-queryset'
 
 VERSION = package_env('VERSION')
 URL = package_env('URL')
